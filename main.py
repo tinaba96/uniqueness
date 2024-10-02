03f1ead8185c6b24f050654ecdf41728eb8d3dd5
tinaba96 <tinaba96.ca@gmail.com>
2024-10-02 23:27:47 +0900

    update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 #import datetime
 #from model import get_data_from_firestore
 
+print('「課題」を入力')
+
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -14,6 +16,7 @@ import numpy as np
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+input_data = str(input())
 texts = []
 
 try:
@@ -40,7 +43,7 @@ except Exception as e:
     print('error')
     print(e)
 
-print(value)
+#print(value)
 
 def text_to_words(text):
     # tagger = MeCab.Tagger('-O chasen')
@@ -82,6 +85,31 @@ def mecab_sep(text):
 
     return words_list
 
+def getUniquePoints(count):
+    match count:
+      case count if 50 <= count:
+        return 0
+      case count if 40 <= count < 50:
+        return 1
+      case count if 35 <= count < 40:
+        return 2
+      case count if 30 <= count < 35:
+        return 3
+      case count if 25 <= count < 30:
+        return 4
+      case count if 20 <= count < 25:
+        return 5
+      case count if 15 <= count < 20:
+        return 6
+      case count if 10 <= count < 15:
+        return 7
+      case count if 5 <= count < 10:
+        return 8
+      case count if 0 < count < 5:
+        return 9
+      case count if 0 == count:
+        return 10
+
 pd_texts = pd.DataFrame(texts, columns=['テキスト'])
 
 all_vecs = calc_vecs(pd_texts['テキスト'])
@@ -90,7 +118,7 @@ all_vecs = calc_vecs(pd_texts['テキスト'])
 #print(all_vecs)
 
 
-input_data = '評価システムがちゃんとしておらず、社員に適切な評価がされていない。'
+#input_data = '評価システムがちゃんとしておらず、社員に適切な評価がされていない。'
 pd_texts = pd.DataFrame([input_data]+texts, columns=['テキスト'])
 
 tot_vecs = calc_vecs(pd_texts['テキスト'])
@@ -100,6 +128,14 @@ similarity = cosine_similarity([tot_vecs[0]], tot_vecs[1:])
 self_similarity = np.array([1])
 pd_texts['類似度'] = np.concatenate([self_similarity, similarity[0]])
 pd_texts.sort_values("類似度", ascending=False)
+
+
+#print(pd_texts)
+
+count_above_threshold = (pd_texts['類似度'] >= 0.3).sum()-1   # 基準が0.3
+
+#print(f'0.5以上の類似度の数: {count_above_threshold}')
+print(f'判定（10が最もユニーク）: {getUniquePoints(count_above_threshold)}')
 
 
 
